49e1e6bbf154538e183c898039fca8cd2ae1e14a
VARUN MYNENI <74002498+mynenivarun@users.noreply.github.com>
2024-05-03 01:36:29 -0500

    Update cons.py

--- cons.py
+++ cons.py
@@ -1,15 +1,15 @@
 dbtype="MySQL"
 
 #DB-1 Details # source
-db1_host = "dmvt-src.cxmou6648xpe.us-east-2.rds.amazonaws.com"
-db1_port = "3306"
-db1_user = "admin"
-db1_pswd = "admin123"
-db1_dtbs = "559pro"
+db1_host = "******"
+db1_port = "******"
+db1_user = "******"
+db1_pswd = "******"
+db1_dtbs = "******"
 
 #DB-2 Details # Target
-db2_host = "dmvt-trg.cfmksow2wz5g.us-west-2.rds.amazonaws.com"
-db2_port = "3306"
-db2_user = "admin"
-db2_pswd = "admin123"
-db2_dtbs = "559pro"
+db2_host = "******"
+db2_port = "******"
+db2_user = "******"
+db2_pswd = "******"
+db2_dtbs = "******"
